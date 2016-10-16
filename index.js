dd539dc816c6846504a88b836b8106ca8c6bead9
Big-Buddy <a.pelletier0x7c9@gmail.com>
2016-10-16 18:44:09 -0400

    change src dir to public, more changes to index

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(path.join(__dirname + '/src')));
-
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname + '/src/home.html'));
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
