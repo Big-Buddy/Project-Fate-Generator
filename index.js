b6d260e4b5660d929c5af9c98a839971586804c4
Big-Buddy <a.pelletier0x7c9@gmail.com>
2016-10-16 17:42:09 -0400

    test ejs file conversion, more configs for index file

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.join(__dirname + '/src')));
-app.use(express.static(path.join(__dirname, '/src/js')));
-app.use(express.static(path.join(__dirname, '/src/css')));
+app.use(express.static(__dirname + '/src/css'));
+app.use(express.static(__dirname + '/src/js'));
+
+app.set('views', __dirname + '/src');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname + '/src/home.html'));
+  response.render('home')
 });
 
 app.listen(app.get('port'), function() {
