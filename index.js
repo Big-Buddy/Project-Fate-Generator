0c2aebd345d96f6b3a99983702318c23df31ef5b
Big-Buddy <a.pelletier0x7c9@gmail.com>
2016-10-16 18:49:04 -0400

    configure port settings in index file

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+var port = process.env.PORT || 5000;
+app.listen(port);
